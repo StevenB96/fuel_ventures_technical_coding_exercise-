20ba3651ce71c66fd9001557f696f8107ea7af4f
StevenB96 <stevenberrisford@gmail.com>
2024-05-18 20:24:17 +0100

    Added a couple of input conditions.

--- src/api.py
+++ src/api.py
@@ -3,20 +3,22 @@ import core
 from schema import SnomedCodeSchema, SearchInputSchema
 
 
-api = Blueprint('api', __name__, url_prefix='/api')
+api = Blueprint("api", __name__, url_prefix="/api")
 
-@api.route('/')
+
+@api.route("/")
 def index():
     return jsonify({"message": "Hello World."})
 
+
 @api.after_request
 def make_json_output(response):
-    """ All our api's will return json. """
-    response.mimetype = 'application/json'
+    """All our api's will return json."""
+    response.mimetype = "application/json"
     return response
 
 
-@api.route("/snomed_code/<description_id>", methods=['GET'])
+@api.route("/snomed_code/<description_id>", methods=["GET"])
 def get_snomed_code(description_id: str):
     """
     Returns the SNOMED code with a matching description_id
@@ -25,12 +27,12 @@ def get_snomed_code(description_id: str):
     try:
         snomed_code_records = core.search_snomed_code_records(
             {
-                'description_id': [str(description_id)],
+                "description_id": [str(description_id)],
             },
-            1
+            1,
         )
 
-        if (snomed_code_records and len(snomed_code_records) == 1):
+        if snomed_code_records and len(snomed_code_records) == 1:
             return snomed_code_records[0]
 
         return []
@@ -38,10 +40,10 @@ def get_snomed_code(description_id: str):
         return f"Error: {e}"
 
 
-@api.route("/snomed_code", methods=['POST'])
+@api.route("/snomed_code", methods=["POST"])
 def add_snomed_code():
     """
-    Should return all the SNOMED codes where the description includes 
+    Should return all the SNOMED codes where the description includes
     all the words in the search string.
     JSON data:
     concept_id: string
@@ -51,19 +53,25 @@ def add_snomed_code():
 
     snomed_code = SnomedCodeSchema().load(request.get_json())
 
+    if False == snomed_code["description_id"].isdigit():
+        return "Error: Description ID must be a number."
+    
+    if False == snomed_code["concept_id"].isdigit():
+        return "Error: Concept ID must be a number."
+
     created = core.post_snomed_code(snomed_code)
 
-    return 'Created SnomedCode!' if created == True else 'Failed to create SnomedCode.'
+    return "Created SnomedCode!" if created == True else "Failed to create SnomedCode."
 
 
-@api.route("/snomed_code/search", methods=['GET'])
+@api.route("/snomed_code/search", methods=["GET"])
 def search_snomed_code():
     """
     Should return all the SNOMED codes where the description includes at least
     n words in the search string.
 
     Query parameters:
-    n - the minimum number of words from the search string that need to be included in the 
+    n - the minimum number of words from the search string that need to be included in the
         SNOMED codes description for it to be considered a match.
     search_string - the search string
     """
@@ -73,12 +81,12 @@ def search_snomed_code():
         search_input_data = SearchInputSchema().load(request.args)
 
         # Access the validated data
-        search_string_q_param = search_input_data.get('search_string', '')
-        n_q_param = search_input_data.get('n', 0)
+        search_string_q_param = search_input_data.get("search_string", "")
+        n_q_param = search_input_data.get("n", 0)
 
         snomed_code_records = core.search_snomed_code_records(
             {
-                'description': search_string_q_param.split(','),
+                "description": search_string_q_param.split(","),
             },
             n_q_param,
         )
